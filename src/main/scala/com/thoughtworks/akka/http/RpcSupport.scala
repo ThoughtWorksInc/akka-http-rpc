dbfee7f98d337198360ff731e500dea62abd13c9
Yang Bo <atryyang@thoughtworks.com>
2019-04-02 14:58:58 +0800

    Fix compilation error after upgrading upickle

--- src/main/scala/com/thoughtworks/akka/http/RpcSupport.scala
+++ src/main/scala/com/thoughtworks/akka/http/RpcSupport.scala
@@ -11,28 +11,28 @@ import akka.http.scaladsl.unmarshalling.Unmarshaller
 /**
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
-trait RpcSupport extends autowire.Server[upickle.Js.Value, upickle.default.Reader, upickle.default.Writer] {
+trait RpcSupport extends autowire.Server[ujson.Value, upickle.default.Reader, upickle.default.Writer] {
 
   implicit private def upickleJsValueMarshaller = {
-    Marshaller.StringMarshaller.wrap[upickle.Js.Value, MessageEntity](MediaTypes.`application/json`)(_.toString)
+    Marshaller.StringMarshaller.wrap[ujson.Value, MessageEntity](MediaTypes.`application/json`)(_.toString)
   }
 
   implicit private def upickleJsValueUnmarshaller = {
-    Unmarshaller.stringUnmarshaller.map(upickle.json.read)
+    Unmarshaller.stringUnmarshaller.map(ujson.read(_))
   }
 
   final def write[Result](r: Result)(implicit writer: upickle.default.Writer[Result]) = {
-    writer.write(r)
+    upickle.default.writeJs(r)
   }
 
-  final def read[Result](p: upickle.Js.Value)(implicit reader: upickle.default.Reader[Result]) = {
-    reader.read(p)
+  final def read[Result](p: ujson.Value)(implicit reader: upickle.default.Reader[Result]) = {
+    upickle.default.read(p)
   }
 
-  final def rpc(packagePrefix: String*)(routes: PartialFunction[Request, Future[upickle.Js.Value]]) = {
+  final def rpc(packagePrefix: String*)(routes: PartialFunction[Request, Future[ujson.Value]]) = {
     path(Segments) { segments =>
-      entity(as[upickle.Js.Value]) {
-        case upickle.Js.Obj(keyValuePairs@_*) =>
+      entity(as[ujson.Value]) {
+        case ujson.Obj(keyValuePairs) =>
           autowire.Core.Request(packagePrefix ++ segments, keyValuePairs.toMap) match {
             case routes.extract(response) =>
               complete {
